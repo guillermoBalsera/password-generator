248b477153cf8eb8a19108109633aea022503688
guillermoBalsera <guimodam@gmail.com>
2024-09-20 17:13:53 +0200

    BiBle

--- src/app/services/generator.service.ts
+++ src/app/services/generator.service.ts
@@ -7,7 +7,10 @@ import {WordsService} from "./words/words.service";
 export class GeneratorService {
   private password: string = '';
 
-  constructor(private words: WordsService) { }
+  private verse: string[] = [];
+
+  constructor(private words: WordsService) {
+  }
 
   public async generate(length: number): Promise<string> {
     length = Math.max(0, Math.min(length, 1000));
@@ -24,25 +27,39 @@ export class GeneratorService {
     return this.password;
   }
 
-private async getChar(): Promise<string> {
-  let type_index: number = Math.floor(Math.random() * 20 + 1);
-  switch (type_index) {
-    case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10: case 11:
-      return this.getRandomNumber(33).toString();
-    case 12: case 13: case 14: case 15:
-      return this.getRandomLowercaseLetter();
-    case 16: case 17:
-      return this.getRandomUppercaseLetter();
-    case 18:
-      return this.getRandomSymbol();
-    case 19:
-      return await this.getRandomSpell();
-    case 20:
-      return await this.getVerse();
-    default:
-      return '';
+  private async getChar(): Promise<string> {
+    let type_index: number = Math.floor(Math.random() * 20 + 1);
+    switch (type_index) {
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+      case 10:
+      case 11:
+        return this.getRandomNumber(33).toString();
+      case 12:
+      case 13:
+      case 14:
+      case 15:
+        return this.getRandomLowercaseLetter();
+      case 16:
+      case 17:
+        return this.getRandomUppercaseLetter();
+      case 18:
+        return this.getRandomSymbol();
+      case 19:
+        return await this.getRandomSpell();
+      case 20:
+        return await this.getVerse();
+      default:
+        return '';
+    }
   }
-}
 
   private getRandomNumber(x: number): number {
     return Math.floor(Math.random() * x);
@@ -77,18 +94,22 @@ private async getChar(): Promise<string> {
 
   private async getVerse(): Promise<string> {
     try {
-      const data: any = await this.words.getRandomVerse().toPromise();
-      let verse = data.text.split(" ");
-      let word: string = verse[this.getRandomNumber(verse.length)]
-        .replaceAll('i', 'imp')
-        .replaceAll('e', '13')
-        .replaceAll('a', '4x4')
-        .replaceAll('o', '02')
-        .replaceAll('u', 'U2u');
-      console.log(word)
-      return word;
+      if (this.verse.length == 0) {
+        const data: any = await this.words.getRandomVerse().toPromise();
+        this.verse = data.text.split(" ");
+      }
+      let word = this.verse[this.getRandomNumber(this.verse.length)]
+        .toLowerCase()
+        .replaceAll('a', 'AcDc')
+        .replaceAll('e', '3m1N3m')
+        .replaceAll('i', 'K1ss')
+        .replaceAll('o', '00h')
+        .replaceAll('u', 'U2');
+
+        console.log(word)
+      return word
     } catch (error) {
-      console.error('Error fetching spells:', error);
+      console.info('No more bible requests');
       return '';
     }
   }
